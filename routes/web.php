1c4a489cd9034ccfdc55bc193d1fbcc5a9edd833
nely247 <neliazizah9477@gmail.com>
2024-08-21 17:12:35 +0700

    Tugas 13

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,13 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'home']);
+Route::get('/',[HomeController::class,'home']);
+Route::get('/register',[AuthController::class,'register']);
+Route::get('/welcome',[AuthController::class,'welcome']);
 
-Route::get('/register', [AuthController::class, 'register']);
-
-Route::get('/welcome', [AuthController::class, 'welcome']);
+route::get('/table',function(){
+    return view('table');
+});
+route::get('/datatable',function(){
+    return view('data-table');
+});
